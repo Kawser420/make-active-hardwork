72e71b9a36894de473d0258e0fe46fbe7ee01685
Kawser420 <muhammadkawser202@gmail.com>
2023-06-13 13:09:14 +0600

    make some file added

---
+++ src/components/Exercise/Exercise.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Exercise = () => {
+    return (
+        <div>
+            <div className="cart-container">
+                <p>exercise cart</p>
+            </div>
+            <div className="cart2-container">
+                <p>exercise cart 2</p>
+            </div>
+        </div>
+    );
+};
+
+export default Exercise;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header-info'>
+            <div className='header'>
+            <img src={logo} alt="" />
+            <h2>Make-Ultra-Club</h2>
+        </div>
+        <h3>Select Today’s Exercise</h3>
+        </div>
+    );
+};
+
+export default Header;
